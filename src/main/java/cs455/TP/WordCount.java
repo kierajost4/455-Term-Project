871fa4731501622be2f8d5c696b3cd27caeea8c3
Kiera Jost <kieraj@rams.colostate.edu>
2022-04-21 17:32:43 -0600

    setup

---
+++ src/main/java/cs455/TP/WordCount.java
@@ -0,0 +1,26 @@
+package cs455.TP;
+
+import java.util.Arrays;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+public class WordCount {
+  public static void main(String[] args) {
+
+    SparkConf sparkConf = new SparkConf().setAppName("Word Count");
+    JavaSparkContext sc = new JavaSparkContext(sparkConf);
+    JavaRDD<String> textFile = sc.textFile("/TP/testdata.txt");
+    JavaPairRDD<String, Integer> counts = textFile
+      .flatMap(s -> Arrays.asList(s.split(" ")).iterator())
+      .mapToPair(word -> new Tuple2<>(word, 1))
+      .reduceByKey((a, b) -> a + b);
+      //file outputs to hdfs
+     counts.saveAsTextFile("/TP/output"); 
+
+  }
+  
+}
