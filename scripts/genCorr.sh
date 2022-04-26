eee65d964c6c5045faf294c5ad5cbff20e1bda4e
wypoulos <wypoulos@rams.colostate.edu>
2022-04-26 13:50:25 -0600

    output to hdfs

--- scripts/genCorr.sh
+++ scripts/genCorr.sh
@@ -2,5 +2,6 @@ echo "Generate the Correlation Matrix"
 gradle build
 
 $HADOOP_HOME/bin/hadoop fs -rm -r /TP/corrMat
+$HADOOP_HOME/bin/hadoop fs -rm -r /TP/stats
 
 $SPARK_HOME/bin/spark-submit --class cs455.TP.GenCorr build/libs/TP-0.1.0.jar
