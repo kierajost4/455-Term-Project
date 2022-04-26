c80fd1a7ebd8dce5698f32a44b2d616e3aa41894
Kiera Jost <kieraj@rams.colostate.edu>
2022-04-26 15:53:56 -0600

    cat data join results

--- scripts/dataJoin.sh
+++ scripts/dataJoin.sh
@@ -4,3 +4,5 @@ gradle build
 $HADOOP_HOME/bin/hadoop fs -rm -r /TP/corrData
 
 $SPARK_HOME/bin/spark-submit --class cs455.TP.DataJoin build/libs/TP-0.1.0.jar
+$HADOOP_HOME/bin/hadoop fs -cat /TP/corrData/part-00001
+$HADOOP_HOME/bin/hadoop fs -cat /TP/corrData/part-00002
